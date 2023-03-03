de77eaed5967090f34bdff43e8b35e0897765551
Muhammad Moin Bukhari <110233225+moinBukhari147@users.noreply.github.com>
2023-03-03 21:59:17 +0500

    Update settings.py

--- quizapp/settings.py
+++ quizapp/settings.py
@@ -137,7 +137,7 @@ EMAIL_USE_TLS = False
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 EMAIL_HOST_USER = 'moinbukhari147@gmail.com'
-EMAIL_HOST_PASSWORD = 'dztknqddleetrwig'
+EMAIL_HOST_PASSWORD = '***********'
 
 
 REST_FRAMEWORK = {
